509a16f2d1c0a47627ca307e916a87ada59a0677
omthakare <42061094+omthakare@users.noreply.github.com>
2023-04-26 03:03:48 +0530

    Update db_login.sh

--- db_login.sh
+++ db_login.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Set database credentials
-DB_USER="username"
-DB_PASS="password"
-DB_HOST="localhost"
+DB_USER="root"
+DB_PASS="Changeme@3262"
+DB_HOST="34.205.30.43"
 DB_PORT="3307"
 DB_NAME="employees"
+
 # Connect to MySQL and run query
 mysql -u $DB_USER -p$DB_PASS -h $DB_HOST -P $DB_PORT $DB_NAME -e "SELECT * FROM employees LIMIT 10;"
+#mysql -u root -pChangeme@3262 -h 34.205.30.43  -P 3307 employees -e "SELECT * FROM employees LIMIT 10;"
